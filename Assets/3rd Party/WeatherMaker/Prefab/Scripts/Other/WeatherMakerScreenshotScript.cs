cdf06c815550c97370d76294f96315b7df6abeb6
TakuRKS <60556726+TakuRKS@users.noreply.github.com>
2021-10-21 07:31:59 -0700

    Terrain edit

---
+++ Assets/3rd Party/WeatherMaker/Prefab/Scripts/Other/WeatherMakerScreenshotScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DigitalRuby.WeatherMaker
+{
+    /// <summary>
+    /// Simple screenshot script for game view. Press R to take screenshots, file is saved to desktop. There is a short delay from pressing R and the screenshot actually saving.
+    /// </summary>
+    public class WeatherMakerScreenshotScript : MonoBehaviour
+    {
+        private int needsScreenshot;
+
+        private void Update()
+        {
+            if (needsScreenshot == 0)
+            {
+                needsScreenshot = Input.GetKeyDown(KeyCode.R) ? 1 : 0;
+            }
+            else if (++needsScreenshot == 64)
+            {
+                ScreenCapture.CaptureScreenshot(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "/screenshot.png");
+                needsScreenshot = 0;
+            }
+        }
+    }
+}
