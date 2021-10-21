460387ce1d8af188444c7cea9817f4e648679f73
redazul <91290695+redazul@users.noreply.github.com>
2021-10-21 02:27:29 -0400

    mouse controller

---
+++ Assets/anim_boss.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class anim_boss : MonoBehaviour
+{
+
+    public Animator _anim;
+    public float speed = 0;
+    public bool crouch = false;
+    public bool climb = false;
+    public GameObject _player;
+    public bool climbAnim = false;
+    public bool moveForward =false;
+    public PlayerInput _playerInput;
+    public ThirdPersonController _moveScript;
+    public CharacterController _chrController;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+
+            public void animKeyFrameClimb()
+    {
+             _anim.SetBool("climb",false);
+            
+    }
+
+
+
+    public void stopClimb()
+    {
+        climbAnim = false;
+        moveForward = true;
+
+    }
+
+    public void stopForwardClimb()
+    {
+        moveForward = false;
+       _moveScript.enabled = true;
+       _playerInput.enabled = true;
+    }
+
+    // public void turnOnController()
+    // {
+        
+    //   _chrController.enabled = true;
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            
+        if(climbAnim==true)
+        {
+                _player.transform.Translate(Vector3.up * Time.deltaTime);
+        }
+
+        if(moveForward==true)
+        {
+            _player.transform.Translate(Vector3.forward*Time.deltaTime);
+        }
+
+                
+            
+
+
+
+       
+
+        if(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if(speed< 1f)
+                {
+                    speed=speed+ 0.003f;
+                }
+                _anim.SetFloat("speed",speed);
+            }else{
+                if(speed<0.6f)
+                {
+                    
+                speed = speed + 0.003f;
+                }
+
+                if(speed>=0.6f)
+                {
+                    
+                speed = speed - 0.003f;
+                }
+                _anim.SetFloat("speed",speed);
+
+            }
+        }else{
+            if(speed>0f)
+            {
+                 speed = speed - 0.003f;
+            }
+               
+                 _anim.SetFloat("speed",speed);
+
+        }
+
+        if(Input.GetKeyDown("c"))
+        {
+            if(crouch==false)
+            {
+            crouch = true;
+            _anim.SetBool("crouch",true);
+            }else{
+            crouch = false;
+            _anim.SetBool("crouch",false);
+
+            }
+
+
+        }
+
+
+        
+    }
+
+    public void animKeyFrameCrouch()
+    {
+             _anim.SetBool("crouch",false);
+    }
+
+
+
+
+
+
+
+}
---
+++ Assets/detect_collision.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class detect_collision : MonoBehaviour
+{
+
+    public Animator _anim;
+    public GameObject _player;
+    public PlayerInput _playerInput;
+    public ThirdPersonController _moveScript;
+    public anim_boss _animScript;
+    public CharacterController _chrController;
+   
+     void OnTriggerEnter(Collider other) {
+         Debug.Log(other.name);
+         if(other.name == "PlayerArmature")
+         {
+             _anim.SetBool("climb",true);
+            
+            
+             _moveScript.enabled = false;
+             //_chrController.enabled = false;
+             _playerInput.enabled = false;
+             _animScript.climbAnim = true;
+
+        
+             
+     
+             
+         }
+            
+     }
+}
