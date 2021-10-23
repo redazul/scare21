6fe9bde1a557e76858fc5a31c831d6b7a78e01ba
Jorge <jorgegabrielcastro@gmail.com>
2021-10-23 09:49:28 -0400

    climb function

--- Assets/Scripts/detect_collision.cs
+++ Assets/Scripts/detect_collision.cs
@@ -15,22 +15,43 @@ public class detect_collision : MonoBehaviour
     public CharacterController _chrController;
    
      void OnTriggerEnter(Collider other) {
-         //Debug.Log(other.name);
-         if(other.name == "PlayerArmature")
+         Debug.Log(other.name);
+         if(other.name== "WallCollider")
          {
-             _anim.SetBool("climb",true);
-            
-            
-             _moveScript.enabled = false;
-             //_chrController.enabled = false;
-             _playerInput.enabled = false;
-             _animScript.climbAnim = true;
 
-        
-             
-     
-             
+             _anim.SetBool("climb",true);
+             _animScript.climbAnim = true;
+             _moveScript.enabled = false;
+             _playerInput.enabled = false;
+         }
+
+          if(other.name== "TopCollider")
+         {
+
+             _anim.SetBool("top",true);
+             _animScript.climbAnim = true;
+             _moveScript.enabled = false;
+             _playerInput.enabled = false;
+         }
+
+        if(other.name== "EndCollider")
+         {
+             Debug.Log("last collider");
+
+             _animScript.climbAnim = false;
+             _animScript.moveForward = false;
+             _anim.SetBool("climb",false);
+              _anim.SetBool("top",false);
+            
+   
+             _moveScript.enabled = true;
+             _playerInput.enabled = true;
          }
             
      }
+
+    //    void OnCollisionExit(Collision collisionInfo)
+    // {
+    //     print("No longer in contact with " + collisionInfo.transform.name);
+    // }
 }
